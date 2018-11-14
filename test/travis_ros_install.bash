a10d9a6a4b503675461542fd72c868f3afc9575e
fukuchi0922 <fukuchi0922@gmail.com>
2018-11-14 04:20:10 +0000

    a

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -5,6 +5,7 @@ pip install catkin_pkg
 pip install empy
 pip install pyyaml
 pip install rospkg
+pip install --upgrade pip
 #ros install
 cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
