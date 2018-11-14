60216c954c04229edb7b11cd8c26518f6cf85f50
fukuchi0922 <fukuchi0922@gmail.com>
2018-11-14 09:29:01 +0000

    testing travis_test_buzzer

--- scripts/buzzer2.py
+++ scripts/buzzer2.py
@@ -5,8 +5,8 @@ from std_msgs.msg import UInt16
 def recv_buzzer(data):
     rospy.loginfo(type(data))
     rospy.loginfo(data.data)
-if __name__ == '__main__':
-    rospy.init_node('buzzer')
-    rospy.Subscriber("buzzer", UInt16, recv_buzzer)
-    rospy.spin()
 
+if __name__ == '__main__':
+    rospy.init_node('buzzer_node')
+    rospy.Subscriber("buzzer_Subscriber", UInt16, recv_buzzer)
+    rospy.spin()
