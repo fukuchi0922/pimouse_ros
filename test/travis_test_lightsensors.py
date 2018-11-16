12ad1d7bf4e00a5d277780f1697250a75163c1a2
fukuchi0922 <fukuchi0922@gmail.com>
2018-11-16 05:22:09 +0000

    edit travis test lightsensors

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -48,7 +48,7 @@ class LightsensorTest(unittest.TestCase):
         self.assertTrue(self.count < c_prev + 4 , "freq does not change")
         self.assertFalse(self.count == c_prev, "subscribe is stopped")
 
-if __name__== '__main__':
+if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_lightsensors')
     rostest.rosrun('pimouse_ros','travis_test_lightsensors',LightsensorTest)
