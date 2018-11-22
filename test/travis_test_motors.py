d22e5b76454c7f9b58d0b83561365f53344bcd13
fukuchi0922 <fukuchi0922@gmail.com>
2018-11-22 01:33:29 +0000

    Collect travistestmotors.py

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -69,11 +69,11 @@ class MotorTest(unittest.TestCase):
 
     def test_put_value_timed(self):
         tm = rospy.ServiceProxy('/timed_motion', TimedMotion)
-        tm = (-321,654,1500)
+        tm(-321,654,1500)
         with open("/dev/rtmotor0","r") ad f:
             data = f.readline()
             self.assertEqual(data,"-321 654 1500\n","value does not written to rtmotor0")
-            
+
 if __name__ == '__main__':
     rospy.init_node('travis_test_motors')
     rostest.rosrun('pimouse_ros','travis_test_motors', MotorTest)
